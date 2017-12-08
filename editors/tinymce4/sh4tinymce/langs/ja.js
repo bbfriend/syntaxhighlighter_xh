c453fa869353cd00d138aab93e68424f30657144
bbfriend <info@u-tec.org>
2017-12-08 20:00:11 +0900

    sh4tinymce for tinumce4
    sh4tinymce for tinumce4

---
+++ editors/tinymce4/sh4tinymce/langs/ja.js
@@ -0,0 +1,13 @@
+tinymce.addI18n('ja',{
+	'SH4TinyMCE - Code Editor'	: 'SyntaxHighlighter SH4TinyMCE',
+	'Insert/Edit Code'			: 'コードの挿入・編集',
+	'Language'					: '言語選択',
+	'Collapse'					: '畳む',
+	'Autolinks'					: '自動リンク',
+	'Gutter'					: '行番号',
+	'Html script'				: 'HTMLを含む',
+	'Toolbar'					: 'ツールバー',
+	'Highlight'					: '特定行をハイライト',
+	'Tab size'					: 'Tab サイズ',
+	'First Line'				: '開始行番号',
+});
